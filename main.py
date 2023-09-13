866ca44cc8cdd1a5e4090397fc33dcfaac740a19
ouali Ahmed <oualiahmed626@gmail.com>
2023-09-13 10:22:55 +0200

    possibilite de changer les couleurs ,leur nombre,le code secret et le nbr de tentatives

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import random
-print("Les couleurs possible : Rouge, Vert, Bleu, Jaune, Mauve, Noir")
 
-LC = ["R", "V", "B", "J", "M", "N"]
-code_c = "".join(random.choice(LC) for _ in range(4))
 
+
+LC = []
+nbr_couleurs = int(input("veuillez donner le  nombre de couleur de la liste : " ))
+nbr_c = int(nbr_couleurs) 
+nbr_elem_code = int(input("veuillez donner le nombre d'element de code secret : "))
+nbr_e_c = int(nbr_elem_code)
+tent = input("veuillez donner le nombre maximal de tentatives : ")
+
+print("les couleurs possibles : Rouge : R / Bleu : B / Vert : V /Jaune : J / Noir : N / Orange : O / Gris : G")
+for i in range (0,nbr_c):
+    couleur = str(input("donner la couleur "+ str(i) + " : "))# Demandez à l'utilisateur de saisir (nbr_e_c) couleurs
+    LC.append(couleur) 
+
+
+code_c = "".join(random.choice(LC) for _ in range(nbr_e_c)) #générer le code couleur secret
 print(code_c)
-score=0
+
+
+
+score=-1
 debut = True
 while debut:
     score+=1
-    if score <=12 :
-        code = input("donner le code couleur  sous la forme des quatre couleurs : ")
+    if score <= int(tent) :
+        code = input("donner le code couleur  sous la forme des  couleurs : ")
         if str(code) == code_c:
             print("Correct : "+ str(code) )
             break
